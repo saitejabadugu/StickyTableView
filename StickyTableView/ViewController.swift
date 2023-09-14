e78d4907252359845e152555995f60ef517a5488
keerthidevipriya <keerthidevipriya@gmail.com>
2023-09-14 12:34:25 +0530

    Initial setup

---
+++ StickyTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StickyTableView
+//
+//  Created by Keerthi Devipriya(kdp) on 14/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
